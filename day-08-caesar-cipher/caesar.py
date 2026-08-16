22485e7dd82e1302fa6124339bafb686c9f5f6aa
zulfianra31 <zulfianra31@users.noreply.github.com>
2026-08-17 00:58:08 +0700

    test 3

--- day-08-caesar-cipher/caesar.py
+++ day-08-caesar-cipher/caesar.py
@@ -9,3 +9,14 @@ angka = ord('A')
 angka_baru = angka + 1
 huruf_baru = chr(angka_baru)
 print(huruf_baru)
+
+
+
+print(10 % 3)
+
+
+print(26 % 26)
+print(27 % 26)
+print(28 % 26)
+
+chr(ord('X') + 3)

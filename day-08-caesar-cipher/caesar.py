110ee2a512b609821b3a9399bfbda2dc0ee41fe1
zulfianra31 <zulfianra31@users.noreply.github.com>
2026-08-16 17:44:46 +0700

    test 2

--- day-08-caesar-cipher/caesar.py
+++ day-08-caesar-cipher/caesar.py
@@ -3,5 +3,9 @@ pesan = input("Masukkan pesan: ")
 print("Pesan kamu:", pesan)
 
 print(ord('A'))
-print(ord('a'))
+print(ord('B'))
 
+angka = ord('A')
+angka_baru = angka + 1
+huruf_baru = chr(angka_baru)
+print(huruf_baru)

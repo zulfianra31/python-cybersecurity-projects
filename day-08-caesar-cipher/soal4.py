193c10fd054bd365add3191e47cb923fef93f289
zulfianra31 <zulfianra31@users.noreply.github.com>
2026-08-27 22:47:26 +0700

    test 8

--- day-08-caesar-cipher/soal4.py
+++ day-08-caesar-cipher/soal4.py
@@ -1,3 +1,27 @@
-print((5 + 21) % 26)
-print((10 + 16) % 26)
-print((0 + 26) % 26)
+def geser_teks(kata, geseran):
+    hasil = ""
+    for huruf in kata:
+        if not huruf.isalpha():
+            hasil = hasil + huruf
+        elif huruf.islower():
+            posisi = ord(huruf) - ord('a')
+            posisi_baru = (posisi + geseran) % 26
+            huruf_baru = chr(posisi_baru + ord('a'))
+            hasil = hasil + huruf_baru
+        else:
+            posisi = ord(huruf) - ord('A')
+            posisi_baru = (posisi + geseran) % 26
+            huruf_baru = chr(posisi_baru + ord('A'))
+            hasil = hasil + huruf_baru
+    return hasil
+
+# baru kode percobaan dekripsi diletakkan di bawah sini
+pesan_asli = "Hello, World!"
+terenkripsi = geser_teks(pesan_asli, 5)
+print("Terenkripsi:", terenkripsi)
+
+hasil_dekripsi_1 = geser_teks(terenkripsi, 21)
+print("Dekripsi (pakai 21):", hasil_dekripsi_1)
+
+hasil_dekripsi_2 = geser_teks(terenkripsi, -5)
+print("Dekripsi (pakai -5):", hasil_dekripsi_2)

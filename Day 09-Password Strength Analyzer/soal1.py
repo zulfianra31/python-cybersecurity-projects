a5ea3f89090e6e72c65b778c613da9b5ac79463a
zulfianra31 <zulfianra31@users.noreply.github.com>
2026-08-31 23:08:40 +0700

    test 10

--- Day 09-Password Strength Analyzer/soal1.py
+++ Day 09-Password Strength Analyzer/soal1.py
@@ -5,3 +5,11 @@ if panjang >= 8:
     print("Panjang: OK")
 else:
     print("Panjang: Terlalu pendek")
+
+    ada_huruf_besar = False   # penanda awal: belum ketemu
+
+for karakter in password:
+    if karakter.isupper():
+        ada_huruf_besar = _______   # <- isi apa di sini?
+
+print(ada_huruf_besar)

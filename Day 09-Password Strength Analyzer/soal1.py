496f62edda5ad988176a244426e4d235a46a9348
zulfianra31 <zulfianra31@users.noreply.github.com>
2026-09-02 21:11:36 +0700

    test 10

--- Day 09-Password Strength Analyzer/soal1.py
+++ Day 09-Password Strength Analyzer/soal1.py
@@ -1,16 +1,21 @@
 password = input("Masukkan password: ")
-panjang = len(password)
 
-if panjang >= 8:
-    print("Panjang: OK")
-else:
-    print("Panjang: Terlalu pendek")
+panjang_cukup = len(password) >= 8
 
-
-ada_huruf_besar = False   # penanda awal: belum ketemu
+ada_huruf_besar = False
+ada_huruf_kecil = False
+ada_angka = False
 
 for karakter in password:
     if karakter.isupper():
-        ada_huruf_besar = True   # begitu ketemu 1 aja, ubah jadi True
+        ada_huruf_besar = True
+    if karakter.islower():
+        ada_huruf_kecil = True
+    if karakter.isdigit():
+        ada_angka = True
 
-print(ada_huruf_besar)
+
+print("Panjang cukup:", panjang_cukup)
+print("Ada huruf besar:", ada_huruf_besar)
+print("Ada huruf kecil:", ada_huruf_kecil)
+print("Ada angka:", ada_angka)

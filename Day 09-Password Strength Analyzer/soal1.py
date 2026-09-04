8e24c2a9eb30e4d44430bb37d9c68cc83754bc50
zulfianra31 <zulfianra31@users.noreply.github.com>
2026-09-04 13:59:04 +0700

    test 10

--- Day 09-Password Strength Analyzer/soal1.py
+++ Day 09-Password Strength Analyzer/soal1.py
@@ -5,6 +5,7 @@ panjang_cukup = len(password) >= 8
 ada_huruf_besar = False
 ada_huruf_kecil = False
 ada_angka = False
+ada_simbol = False
 
 for karakter in password:
     if karakter.isupper():
@@ -13,14 +14,27 @@ for karakter in password:
         ada_huruf_kecil = True
     if karakter.isdigit():
         ada_angka = True
-
+    if not karakter.isalnum():
+        ada_simbol = True
 
 print("Panjang cukup:", panjang_cukup)
 print("Ada huruf besar:", ada_huruf_besar)
 print("Ada huruf kecil:", ada_huruf_kecil)
 print("Ada angka:", ada_angka)
+print("Ada simbol:", ada_simbol)
 
-print('a'.isalnum())
-print('5'.isalnum())
-print('!'.isalnum())
-print('@'.isalnum())
+print(True + True)
+print(True + False)
+print(False + False)
+
+skor = panjang_cukup + ada_huruf_besar + ada_huruf_kecil + ada_angka + ada_simbol
+print("Skor:", skor)
+
+if skor <= 2:
+    label = "Lemah"
+elif skor <= 4:
+    label = "Sedang"
+else:
+    label = "Kuat"
+
+print("Kekuatan password:", label)

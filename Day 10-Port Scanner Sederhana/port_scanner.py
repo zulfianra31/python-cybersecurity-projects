155a842ade1b16b83d0f3e2d2b3e1886df0ca58e
zulfianra31 <zulfianra31@users.noreply.github.com>
2026-09-07 22:08:07 +0700

    test 11

--- Day 10-Port Scanner Sederhana/port_scanner.py
+++ Day 10-Port Scanner Sederhana/port_scanner.py
@@ -1,13 +1,13 @@
 import socket
 
 target = "127.0.0.1"
-port = 445  # port yang biasanya dipakai Windows (file sharing)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-hasil = s.connect_ex((target, port))
-
-print(hasil)
-s.close()
-
-for angka in range(1, 5):
-    print(angka)
+for port in range(1, 1025):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.settimeout(0.5)  # <- baris baru: nunggu maksimal 0.5 detik per port
+    hasil = s.connect_ex((target, port))
+    
+    if hasil == 0:
+        print("Port", port, "TERBUKA")
+    
+    s.close()

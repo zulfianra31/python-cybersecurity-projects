7cd2204ad57db69af1757c1f017290b20b38b8c0
zulfianra31 <zulfianra31@users.noreply.github.com>
2026-09-07 22:03:08 +0700

    test 11

--- Day 10-Port Scanner Sederhana/port_scanner.py
+++ Day 10-Port Scanner Sederhana/port_scanner.py
@@ -1,7 +1,7 @@
 import socket
 
 target = "127.0.0.1"
-port = 80
+port = 445  # port yang biasanya dipakai Windows (file sharing)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 hasil = s.connect_ex((target, port))

a9cff3fb7dcbee4865219b4f5ec3d32abdcdef62
zulfianra31 <zulfianra31@users.noreply.github.com>
2026-09-11 13:25:25 +0700

    test 12

--- Day 11-Log File Analyzer/log_analyzer.py
+++ Day 11-Log File Analyzer/log_analyzer.py
@@ -1,4 +1,10 @@
-with open("sample.log", "r") as file:
-    isi = file.read()
+with open(r"E:\all project\project 2 (cyber)\Day 11-Log File Analyzer\sample.log", "r") as file:
+    baris_baris = file.readlines()
 
-print(isi)
+print(len(baris_baris))
+print(baris_baris[0])
+print(baris_baris[1])
+
+kalimat = "LOGIN_FAILED user=admin ip=203.0.113.55"
+print("LOGIN_FAILED" in kalimat)
+print("LOGIN_SUCCESS" in kalimat)

23d1fe6a9791e89e75585560e1d37172f4211a7d
zulfianra31 <zulfianra31@users.noreply.github.com>
2026-09-11 14:27:59 +0700

    tets 12

--- Day 11-Log File Analyzer/log_analyzer.py
+++ Day 11-Log File Analyzer/log_analyzer.py
@@ -1,10 +1,19 @@
 with open(r"E:\all project\project 2 (cyber)\Day 11-Log File Analyzer\sample.log", "r") as file:
     baris_baris = file.readlines()
 
-print(len(baris_baris))
-print(baris_baris[0])
-print(baris_baris[1])
+# print(len(baris_baris))
+# print(baris_baris[0])
+# print(baris_baris[1])
 
-kalimat = "LOGIN_FAILED user=admin ip=203.0.113.55"
-print("LOGIN_FAILED" in kalimat)
-print("LOGIN_SUCCESS" in kalimat)
+# kalimat = "LOGIN_FAILED user=admin ip=203.0.113.55"
+# print("LOGIN_FAILED" in kalimat)
+# print("LOGIN_SUCCESS" in kalimat)
+
+# for baris in baris_baris:
+#     if "LOGIN_FAILED" in baris:
+#         print(baris)
+
+
+baris = "22026-09-08 08:14:25 LOGIN_FAILED user=admin ip=203.0.113.55"
+potongan = baris.split("ip=")
+print(potongan)

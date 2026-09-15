27af004f841e8d4915d430989e8a94380aab3574
zulfianra31 <zulfianra31@users.noreply.github.com>
2026-09-15 20:49:04 +0700

    test 12

--- Day 11-Log File Analyzer/Dictionary.py
+++ Day 11-Log File Analyzer/Dictionary.py
@@ -5,9 +5,9 @@ print(catatan)
 catatan["203.0.113.55"] = catatan["203.0.113.55"] + 1
 print(catatan)
 
-catatan = {}
-catatan["1.2.3.4"] = 1
-print(catatan)
+# catatan = {}
+# catatan["9.9.9.9"] = catatan["9.9.9.9"] + 1
 
-catatan["1.2.3.4"] = catatan["1.2.3.4"] + 1 
-print(catatan) 
+catatan = {"203.0.113.55": 2}
+print("203.0.113.55" in catatan)
+print("9.9.9.9" in catatan)
